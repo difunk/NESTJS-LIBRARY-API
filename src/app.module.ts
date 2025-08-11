e550c50647a1ff0a68f7c5c7c68cfd71c003be17
Dirk <so0mewhat2@gmail.com>
2025-08-11 12:36:54 +0200

    chore: migrate to PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,33 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './books/entity/book.entity';
 import { UsersModule } from './users/users.module';
 import { User } from './users/entity/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     BooksModule,
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'database.sqlite',
+      type: 'postgres',
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'mysecretpassword',
+            database: 'nestjs-library-api-db',
+          }),
       entities: [Book, User],
       synchronize: true,
+      ssl:
+        process.env.NODE_ENV === 'production'
+          ? { rejectUnauthorized: false }
+          : false,
       migrations: [__dirname + '/../database/migrations/*.{js,ts}'], // Path to your migration files
       migrationsRun: false, // Set to true if you want migrations to run on app start (careful in prod!)
       logging: ['query', 'error'],
